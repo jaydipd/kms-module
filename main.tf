f8634c71077f23328a08649a75429f0243745f7a
jaydipd <jay.android14@gmail.com>
2025-06-16 01:33:46 +0530

    adding kms alias module

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "aws_kms_key" "aws_kms" {
   enable_key_rotation = var.is_rotataion_enabled
   deletion_window_in_days = var.deletion_window_in_days
 }
+
+resource "aws_kms_alias" "kms_key_alias" {
+  name = "alias/${var.alias_name}"
+  target_key_id = aws_kms_key.aws_kms.id
+}
--- variable.tf
+++ variable.tf
@@ -12,3 +12,8 @@ variable "deletion_window_in_days" {
   description = "after how many days after you want to delete the key"
   type = number
 }
+
+variable "alias_name" {
+  description = "alias is used to point to actual key id"
+  type = string
+}
