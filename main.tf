af9c2ba67bee18c79fa9c4350751cc89e00fc3c9
jaydipd <jay.android14@gmail.com>
2025-06-16 01:47:29 +0530

    pointed resource to this

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_kms_key" "aws_kms" {
+resource "aws_kms_key" "this" {
   description = var.description
   enable_key_rotation = var.is_rotataion_enabled
   deletion_window_in_days = var.deletion_window_in_days
 }
 
-resource "aws_kms_alias" "kms_key_alias" {
+resource "aws_kms_alias" "this" {
   name = "alias/${var.alias_name}"
   target_key_id = aws_kms_key.aws_kms.id
 }
