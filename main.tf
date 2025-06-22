14807a6913ce16a664d1d3f5f7646b7e0b149226
jaydipd <jay.android14@gmail.com>
2025-06-22 17:39:10 +0530

    changed module name

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_kms_key" "this" {
+resource "aws_kms_key" "aws_kms_key_module" {
   description = var.description
   enable_key_rotation = var.is_rotataion_enabled
   deletion_window_in_days = var.deletion_window_in_days
 }
 
-resource "aws_kms_alias" "this" {
+resource "aws_kms_alias" "aws_kms_alias_module" {
   name = "alias/${var.alias_name}"
-  target_key_id = aws_kms_key.this.id
+  target_key_id = aws_kms_key.aws_kms_key_module.id
 }
--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "key_id" {
   description = "The globally unique identifier for the KMS key"
-  value       = aws_kms_key.this.key_id
+  value       = aws_kms_key.aws_kms_key_module.key_id
 }
 
 output "key_arn" {
   description = "The ARN of the KMS key"
-  value       = aws_kms_key.this.arn
+  value       = aws_kms_key.aws_kms_key_module.arn
 }
 
 output "alias_name" {
   description = "The name of the KMS alias"
-  value       = aws_kms_alias.this.name
+  value       = aws_kms_alias.aws_kms_alias_module.name
 }
 
 output "alias_arn" {
   description = "The ARN of the KMS alias"
-  value       = aws_kms_alias.this.arn
+  value       = aws_kms_alias.aws_kms_alias_module.arn
 }
 
 output "key_policy" {
   description = "The policy document attached to the KMS key"
-  value       = aws_kms_key.this.policy
+  value       = aws_kms_key.aws_kms_key_module.policy
 }
